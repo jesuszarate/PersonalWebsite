61ddb97b736be254bce0c5dda14d74728b107777
zarate8 <jesus.zarate.eng@gmail.com>
2015-12-20 15:24:48 -0700

    first commit

---
+++ zaratEngineering/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zaratEngineering.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static zaratEngineering.Models.WishItemImage model = 
+            new zaratEngineering.Models.WishItemImage();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Contacts()
+        {
+            ViewBag.Message = "Contact page.";
+
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult myWishList()
+        {
+            return View(model);
+        }
+
+        public ActionResult FullImage(String path)
+        {
+            return View(model);
+        }
+
+        public ActionResult javeWL()
+        {
+            return View(model);
+        }        
+    }
+}
---
+++ zaratEngineering/Models/WishItemImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zaratEngineering.Models
+{
+    public class WishItemImage
+    {
+        String imagePath = 
+            "~/Images/s6-edge.jpg"; 
+
+        public String ImagePath
+        {
+            get { return imagePath; }
+            set { imagePath = value; }
+        }
+    }
+}
